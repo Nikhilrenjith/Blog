4f31b77b5b1c42f5164d4e14d8992951cfebda19
Nikhilrenjith <42229967+Nikhilrenjith@users.noreply.github.com>
2023-12-26 14:43:15 +0530

    updated logina nd signup functionality

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -1,13 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  const login = async (ev) => {
+    ev.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/login",
+        {
+          username,
+          password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+
+      if (response.status === 200) {
+        alert("Login success");
+        setRedirect(true);
+      } else {
+        alert("Wrong credentials");
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+      alert("An error occurred during login");
+    }
+  };
+
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
+
   return (
     <div>
-      <form className="login">
+      <form className="login" onSubmit={login}>
         <h1>Login</h1>
-        <input type="text" placeholder="username" />
-        <input type="password" placeholder="password" />
-        <button>Login</button>
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
       </form>
     </div>
   );
--- src/Auth/Signup.jsx
+++ src/Auth/Signup.jsx
@@ -1,13 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Signup = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const register = async (ev) => {
+    ev.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/register",
+        {
+          username,
+          password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log(response.data);
+      if (
+        response.data &&
+        response.data.message === "User registered successfully"
+      ) {
+        alert("User registered successfully!");
+        console.log("Username:", username);
+        console.log("Password:", password);
+      }
+    } catch (error) {
+      alert("Error during registration:");
+    }
+  };
+
   return (
     <div>
       <form className="register">
         <h1>Register</h1>
-        <input type="text" placeholder="username" />
-        <input type="password" placeholder="password" />
-        <button>Register</button>
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="button" onClick={register}>
+          Register
+        </button>
       </form>
     </div>
   );
